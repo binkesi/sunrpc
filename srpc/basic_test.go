3d30090a20f43711c80b2eaaa2d6b9ac74135e00
binkesi <sjtusgn@163.com>
2021-10-25 15:58:25 +0800

    Implement local rpc server of double number.

--- srpc/basic_test.go
+++ srpc/basic_test.go
@@ -2,6 +2,9 @@ package srpc
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"net/rpc"
 	"testing"
 )
 
@@ -9,4 +12,30 @@ import (
 
 func TestRpc(t *testing.T) {
 	fmt.Println("This is a test.")
+}
+
+func TestDouble(t *testing.T) {
+	rpc.RegisterName("Server", new(Server))
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		log.Fatal("Listen tcp err:", err)
+	}
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatal("Connection error:", err)
+	}
+	rpc.ServeConn(conn)
+}
+
+func TestClient(t *testing.T) {
+	client, err := rpc.Dial("tcp", "localhost:1234")
+	if err != nil {
+		log.Fatal("Dial error:", err)
+	}
+	var reply string
+	err = client.Call("Server.DoubleNum", "18", &reply)
+	if err != nil {
+		log.Fatal("Server error:", err)
+	}
+	fmt.Printf("Double result is: %s\n", reply)
 }
--- srpc/server.go
+++ srpc/server.go
@@ -1,3 +1,18 @@
 package srpc
 
+import (
+	"log"
+	"strconv"
+)
+
 type Server struct{}
+
+func (server *Server) DoubleNum(request string, reply *string) error {
+	if num, err := strconv.Atoi(request); err != nil {
+		log.Println(err.Error())
+		return err
+	} else {
+		*reply = strconv.Itoa(2 * num)
+	}
+	return nil
+}

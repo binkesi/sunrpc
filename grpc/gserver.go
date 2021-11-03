f397f86eb7a0377d5d0ef68a35dc9859ffec9084
binkesi <sjtusgn@163.com>
2021-11-03 11:20:31 +0800

    Add panic catch.

--- grpc/gserver.go
+++ grpc/gserver.go
@@ -2,6 +2,7 @@ package grpc
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log"
 	"strings"
@@ -13,6 +14,11 @@ import (
 
 func filter(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 	log.Println("filter:", info)
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("panic: %v", r)
+		}
+	}()
 	return handler(ctx, req)
 }
 
